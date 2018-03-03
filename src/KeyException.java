6b1d95912411c6686dabb0ff8cfc8be04ab315db
MrMaxweII <34688939+MrMaxweII@users.noreply.github.com>
2018-03-03 19:27:40 +0100

    Update KeyException.java

--- src/KeyException.java
+++ src/KeyException.java
@@ -4,13 +4,13 @@
 
 public class KeyException extends Exception
 {
-		public KeyException()
-		{
-			super("Ich bin deine Exception");
-		}
-	
-		public KeyException(String f)
-		{
-			super(f);
-		}
+	public KeyException()
+	{
+		super("Ich bin deine Exception");
+	}
+
+	public KeyException(String f)
+	{
+		super(f);
+	}
 }
